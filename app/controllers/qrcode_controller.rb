db37e111c8bd5e3e91074b6ff639e453f802bb16
Bob Matcuk <bmatcuk@gmail.com>
2013-02-07 15:20:41 -1000

    Some styling

--- app/controllers/qrcode_controller.rb
+++ app/controllers/qrcode_controller.rb
@@ -1,8 +1,13 @@
 class QrcodeController < ApplicationController
   
   def show
-    @code = QRCode.find_by_shortcode params[:shortcode]
+    #@code = QRCode.find_by_shortcode params[:shortcode]
     #raise ActionController::RoutingError.new('Not Found') if @code.nil?
+    begin
+      render params[:shortcode]
+    rescue ActionView::MissingTemplate
+      render
+    end
   end
   
 end
