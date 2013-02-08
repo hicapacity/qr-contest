814ae00a64741dfe4d967f0f8972271f0862c60a
Bob Matcuk <bmatcuk@gmail.com>
2013-02-07 23:40:09 -1000

    more styling and facebook login

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,11 +1,17 @@
 $(function() {
   $('#fblogin').on('click', function() {
+    $('#login').hide();
+    $('#loading').fadeIn();
     FB.login(function (response) {
+      $('#loading').hide();
       if (response.authResponse) {
-      
+        $('#main-content').fadeIn();
       } else {
-        
+        $('#new-visitor').hide();
+        $('#denied').show();
+        $('#login').fadeIn();
       }
+      console.debug(response);
     });
   });
 });
@@ -14,8 +20,12 @@ window.fbInitialized = function() {
   FB.getLoginStatus(function (response) {
     if (response.status == 'connected') {
       // Already connected and authorized
+      $('#loading').hide();
+      $('#main-content').fadeIn();
     } else {
       // Not authorized or not logged in
+      $('#loading').hide();
+      $('#login').fadeIn();
     }
     console.debug(response);
   });
