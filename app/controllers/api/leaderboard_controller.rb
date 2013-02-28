06972c374040274344946def02017d590c0cbec4
Bob Matcuk <bmatcuk@gmail.com>
2013-02-27 19:33:41 -1000

    corrected issue with api

--- app/controllers/api/leaderboard_controller.rb
+++ app/controllers/api/leaderboard_controller.rb
@@ -3,14 +3,14 @@ module Api
     
     def index
       render :json => {
-        :timestamp => DateTime.now.iso8601,
+        :timestamp => DateTime.now.utc.iso8601,
         :users => User.all.map do |u|
           {
             :id => u.fbid,
             :name => u.username,
             :pic => "//graph.facebook.com/#{u.use_profile_pic ? u.fbid : 1}/picture",
             :codes => u.qr_codes.count,
-            :stamp => (u.last_qrcode_found_at.nil?() ? 0 : u.last_qrcode_found_at.iso8601)
+            :stamp => (u.last_qrcode_found_at.nil?() ? 0 : u.last_qrcode_found_at.utc.iso8601)
           }
         end
       }
@@ -18,21 +18,21 @@ module Api
     
     def updates_since
       render :json => {
-        :timestamp => DateTime.now.iso8601,
+        :timestamp => DateTime.now.utc.iso8601,
         :new_users => User.where('created_at > ?', params[:stamp]).map do |u|
           {
             :id => u.fbid,
             :name => u.username,
             :pic => "//graph.facebook.com/#{u.use_profile_pic ? u.fbid : 1}/picture",
             :codes => u.qr_codes.count,
-            :stamp => (u.last_qrcode_found_at.nil?() ? 0 : u.last_qrcode_found_at.iso8601)
+            :stamp => (u.last_qrcode_found_at.nil?() ? 0 : u.last_qrcode_found_at.utc.iso8601)
           }
         end,
         :updates => User.where('last_qrcode_found_at > ?', params[:stamp]).map do |u|
           {
             :id => u.fbid,
             :codes => u.qr_codes.count,
-            :stamp => u.last_qrcode_found_at.iso8601
+            :stamp => u.last_qrcode_found_at.utc.iso8601
           }
         end
       }
